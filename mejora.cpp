1ea70d69167860199550ddcb146f0ce313fc1b3b
aldo_bedrij <wbedrij@gmail.com>
2012-06-09 15:17:28 -0300

    Visualizacion de los canales

---
+++ mejora.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+#include "aux.h"
+
+int main(int argc, char **argv){
+	cv::VideoCapture capture(0);
+	cv::Mat frame;
+
+	std::string 
+		windows[] = {"hue", "saturation", "value"};
+	for(size_t K=0; K<3; ++K)
+		cv::namedWindow(windows[K], CV_WINDOW_KEEPRATIO);
+
+	while( true ){
+		capture>>frame;
+		std::vector<cv::Mat> hsv;
+		cv::cvtColor(frame, frame, CV_BGR2HSV);
+		cv::split(frame, hsv);
+
+		for(size_t K=0; K<hsv.size(); ++K)
+			cv::imshow(windows[K], hsv[K]);
+		if(cv::waitKey(30)>=0) break;
+	}
+	return 0;
+}
+
+
